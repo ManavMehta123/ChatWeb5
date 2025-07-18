9744e0b694b67a7a4a867da68f3780dbd900430f
ManavMehta123 <128663812+ManavMehta123@users.noreply.github.com>
2025-07-18 20:05:23 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Verify from "./pages/Verify";
-import { UserData } from "./context/UserContext";
 import { LoadingBig } from "./components/Loading";
 
 const App = () => {
